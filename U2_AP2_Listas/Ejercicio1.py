459341a01356c448715d88d72a4f4f84f46304cb
Jose Valverde <jovall@floridauniversitaria.es>
2022-02-12 16:23:12 +0100

    Mejora de codigo

--- U2_AP2_Listas/Ejercicio1.py
+++ U2_AP2_Listas/Ejercicio1.py
@@ -1,3 +1,16 @@
+# Escribe un programa que vaya pidiendo palabras al usuario hasta que este introduzca "FIN" y, tras ello, muestre las palabras en orden alfabético inverso.
+
+import sys
+import platform
+import subprocess
+# Borramos terminal dependiendo el Sistema Operativo
+if platform.system() == "Windows":
+    from os import system
+    system("cls")
+else:
+    from os import system
+    system("clear")
+
 palabras = []
 print("Escribe FIN para finalizar.")
 
